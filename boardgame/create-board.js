925fee6dd2056fec0014b76da212b8639d681c45
Nikos-VR <n.vasilopoulos@go.uop.gr>
2026-02-20 11:50:23 +0200

    Update create-board.js

--- boardgame/create-board.js
+++ boardgame/create-board.js
@@ -64,6 +64,7 @@ function createUnits(container) {
 
             unit.setAttribute('scale', GAME.unitsScale);
             unit.setAttribute('position', boardSize[0] * (x - BOARD[0].length / 2 + 0.5) + ' ' + unitSize[1] / 2 + ' ' + boardSize[2] * (z - BOARD.length / 2 + 0.5));
+            unit.setAttribute('networked', '');
 
             unit.classList.add('unit');
             unit.classList.add('interactive');
@@ -76,4 +77,5 @@ function createUnits(container) {
             container.appendChild(unit);
         }
     }
+
 }
